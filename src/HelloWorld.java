5ccbda1f309eb3b92f74707d2571bcacdf0b8a29
yashj0209 <53044482+yashj0209@users.noreply.github.com>
2022-06-20 13:24:34 +0530

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
     public static void main(String args[])
     {
-        System.out.println("Hello, World, push test");
+        System.out.println("Hello, World, test");
     }
 }
